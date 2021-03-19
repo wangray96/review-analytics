94c24f74986666334ebc8eb4d993c6a40b92d5c0
Ray <ray@pc41.wireless55.tku.edu.tw>
2021-03-19 19:09:57 +0800

    留言分析1版

--- read.py
+++ read.py
@@ -6,5 +6,12 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
+print('檔案讀取完畢, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言的平均長度為', sum_len/len(data))
+ 
 
 
